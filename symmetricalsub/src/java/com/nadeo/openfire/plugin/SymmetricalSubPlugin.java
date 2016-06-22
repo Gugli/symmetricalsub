c0b501eb2b59c03b90765f825e34643f01df6724
Sylvain "Gugli" Guglielmi <sylvain@gugli.me>
2016-06-22 17:29:55 +0200

    First real implem of the plugin.

--- symmetricalsub/src/java/com/nadeo/openfire/plugin/SymmetricalSubPlugin.java
+++ symmetricalsub/src/java/com/nadeo/openfire/plugin/SymmetricalSubPlugin.java
@@ -23,12 +23,17 @@ import org.jivesoftware.openfire.container.PluginManager;
 import org.jivesoftware.openfire.interceptor.InterceptorManager;
 import org.jivesoftware.openfire.interceptor.PacketInterceptor;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
+import org.jivesoftware.openfire.roster.RosterManager;
+import org.jivesoftware.openfire.roster.Roster;
+import org.jivesoftware.openfire.roster.RosterItem;
+import org.jivesoftware.openfire.user.UserNotFoundException;
 import org.jivesoftware.openfire.session.Session;
 import org.jivesoftware.util.JiveGlobals;
 import org.xmpp.packet.JID;
 import org.xmpp.packet.Packet;
 import org.xmpp.packet.Presence;
 import org.xmpp.packet.Presence.Type;
+import org.jivesoftware.util.Log;
 
 import java.io.File;
 import java.util.*;
@@ -49,12 +54,14 @@ public class SymmetricalSubPlugin implements Plugin {
 
     private PresenceRouter router;
     private String serverName;
+    private RosterManager rosterManager;
     
     public SymmetricalSubPlugin() {
         
         XMPPServer server = XMPPServer.getInstance();
         router = server.getPresenceRouter();
         serverName = server.getServerInfo().getXMPPDomain();
+        rosterManager = server.getRosterManager();
     }
 
     public void initializePlugin(PluginManager manager, File pluginDirectory) {
@@ -85,33 +92,83 @@ public class SymmetricalSubPlugin implements Plugin {
                 return;
             }
 
-            if ((packet instanceof Presence) && !incoming && !processed) {
+            if ((packet instanceof Presence) && !processed) {
                 Presence presencePacket = (Presence) packet;
 
                 Type presenceType = presencePacket.getType();
-                if (presenceType != null && presenceType.equals(Presence.Type.subscribe)) {
-                    JID toJID = presencePacket.getTo();
-                    JID fromJID = presencePacket.getFrom();
-                    String toNode = toJID.getNode();
+                if (presenceType == null ) 
+                    return;
+                
+                JID toJID = presencePacket.getTo();
+                JID fromJID = presencePacket.getFrom();
+ 
+                if (mode.equals(LOCAL)) {
+                    String toDomain = toJID.getDomain();
+                    String fromDomain = fromJID.getDomain();
 
-                    if (mode.equals(LOCAL)) {
-                        String toDomain = toJID.getDomain();
-                        String fromDomain = fromJID.getDomain();
-
-                        if (!toDomain.equals(serverName) || !fromDomain.equals(serverName)) {
-                            return;
+                    if (!toDomain.equals(serverName) || !fromDomain.equals(serverName)) {
+                        return;
+                    }
+                } 
+                  
+                if(incoming) {
+                    if(presenceType.equals(Presence.Type.subscribe)) {
+                        
+                        RosterItem rosterItem = null;
+                        try {
+                            String toNode = toJID.getNode();
+                            Roster roster = rosterManager.getRoster(toNode);
+                            rosterItem = roster.getRosterItem(fromJID);
+                        } catch( UserNotFoundException e) {
+                            // user or contact not found, it's ok, just do nothing
                         }
-                    } 
-                      
-                    // Simulate that the target user has accepted the presence subscription request
-                    Presence presence = new Presence();
-                    presence.setType(Presence.Type.subscribed);
-
-                    presence.setTo(fromJID);
-                    presence.setFrom(toJID);
-                    router.route(presence);
-
-                    throw new PacketRejectedException();
+                        
+                        boolean userIsSuscribedOrPending = 
+                            (rosterItem != null) &&
+                            (   rosterItem.getSubStatus() == RosterItem.SUB_BOTH ||    // Should not happen (we're already subscribed, so why ask again ?)
+                                rosterItem.getSubStatus() == RosterItem.SUB_TO ||      // User is subscribed to contact
+                                rosterItem.getAskStatus() == RosterItem.ASK_SUBSCRIBE  // User has asked subscription to contact
+                            );
+                        if(userIsSuscribedOrPending) {
+                            // Case 2 in readme
+                            // user is suscribed (or pending) to this contact, and contact has sent a sub request : accept automatically
+                            Log.info("SymmetricalSub #2 : " + toJID.toBareJID() + " has recieved an subscribe request from " + fromJID.toBareJID() + " and is subscribed : accept automatically" );
+                            Presence presence = new Presence();
+                            presence.setType(Presence.Type.subscribed);
+                            presence.setTo(fromJID);
+                            presence.setFrom(toJID);
+                            router.route(presence);
+                            throw new PacketRejectedException();
+                        }
+                    } else if (presenceType.equals(Presence.Type.unsubscribe)) {
+                        // Case 3 in readme
+                        // User has recieved an unsubscribe request : accept automatically + send unsubscribe back
+                        Log.info("SymmetricalSub #3 : " + toJID.toBareJID() + " has recieved an unsubscribe request from " + fromJID.toBareJID() + " : accept automatically + send unsubscribe back" );
+                        Presence presence2 = new Presence();
+                        presence2.setType(Presence.Type.unsubscribe);
+                        presence2.setTo(fromJID);
+                        presence2.setFrom(toJID);
+                        router.route(presence2);
+                        
+                        Presence presence1 = new Presence();
+                        presence1.setType(Presence.Type.unsubscribed);
+                        presence1.setTo(fromJID);
+                        presence1.setFrom(toJID);
+                        router.route(presence1);
+                        
+                        throw new PacketRejectedException();
+                    }
+                } else {
+                    if ( presenceType.equals(Presence.Type.subscribed) ) {
+                        // Case 1 in readme 
+                        // User accepts a sub request : Also send an "opposite" sub request 
+                        Log.info("SymmetricalSub #1 : " + fromJID.toBareJID() + " has accepted an subscribe request from " + toJID.toBareJID() + " : send an opposite sub request" );
+                        Presence presence = new Presence();
+                        presence.setType(Presence.Type.subscribe);
+                        presence.setTo(fromJID);
+                        presence.setFrom(toJID);
+                        router.route(presence);
+                    }
                 }
             }
         }
